7f4e2ac8e00b6dc09baae26fd0c5bf52ae7d3359
tristan <tristan@MacBook-Pro-de-tristan.local>
2021-01-04 12:01:20 +0100

    mongodb for heroku on prod

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,15 +1,14 @@
 module.exports = ({ env }) => ({
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        uri: env('DATABASE_URI'),
+      },
+      options: {
+        ssl: true,
       },
     },
-  });
+  },
+});
