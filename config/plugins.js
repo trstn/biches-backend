5d95c66ec5c1bf67907de1726ef511c3ceaa3ec8
tristan <tristan@MacBook-Pro-de-tristan.local>
2021-01-04 10:54:05 +0100

    env

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('trstn'),
-        api_key: env('258645841846429'),
-        api_secret: env('AV7_i6TYgIEhyQH4MmnIgoqBZ80'),
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
       },
     },
     // ...
